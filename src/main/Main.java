bda882c46314f1f4d7a5ddbbf18319c7519a66da
ramzan1990 <umarov256@gmail.com>
2016-12-21 13:59:38 +0300

    First commit

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello1");
+        System.out.println("Hello2");
+        System.out.println("Hello3");
+    }
+}
