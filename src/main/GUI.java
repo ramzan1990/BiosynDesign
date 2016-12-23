9397e4223b9f27e29676399935bb87d39e54545f
ramzan1990 <umarov256@gmail.com>
2016-12-23 22:54:47 +0300

    GUI

---
+++ src/main/GUI.java
@@ -0,0 +1,495 @@
+package main;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.PropertyVetoException;
+
+import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.event.InternalFrameEvent;
+import javax.swing.event.InternalFrameListener;
+
+public class GUI extends JFrame {
+
+    private static final int panelMargin = 6;
+    private JMenu File, HelpM, Options, Window;
+    private JMenuItem ClearConsole, Exit,   Save, Help, About, NewProject, SaveAs, OpenProject, Tile, Cascade;
+    private JCheckBoxMenuItem HideDataPanel, HideTools, HideConsole;
+    ButtonGroup transformGroup;
+    private JPanel dataSelectPanel, consolePanel, dataTransformPanel;
+    private JPanel dataPanel;
+    private JToolBar toolsPanel;
+    private JDesktopPane workSpacePanel;
+    private JMenuBar menu;
+    public JTextArea consoleArea;
+    private JScrollPane consoleScroll;
+    private JLabel newProject, openProject, saveProject;
+
+    public GUI() {
+        // <editor-fold defaultstate="collapsed" desc="menu">
+        ClearConsole = new JMenuItem("Clear console");
+        HideDataPanel = new JCheckBoxMenuItem("Data Panel");
+        HideTools = new JCheckBoxMenuItem("Tools Panel");
+        HideConsole = new JCheckBoxMenuItem("Console");
+        menu = new JMenuBar();
+        File = new JMenu("File");
+        HelpM = new JMenu("Help");
+        Options = new JMenu("Options");
+        Window = new JMenu("Window");
+        Exit = new JMenuItem("Exit");
+        Save = new JMenuItem("Save");
+        Help = new JMenuItem("Help");
+        About = new JMenuItem("About");
+        NewProject = new JMenuItem("New Project");
+        SaveAs = new JMenuItem("Save As");
+        OpenProject = new JMenuItem("Open Project");
+        Tile = new JMenuItem("Tile");
+        Cascade = new JMenuItem("Cascade");
+
+        NewProject.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+
+            }
+        });
+
+        File.add(NewProject);
+        File.add(OpenProject);
+        File.addSeparator();
+        File.add(Save);
+        File.add(SaveAs);
+        File.addSeparator();
+        File.add(Exit);
+        OpenProject.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+
+            }
+        });
+        SaveAs.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+
+            }
+        });
+        Exit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                System.exit(0);
+            }
+        });
+
+        Save.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+
+            }
+        });
+
+        
+        HideConsole.setSelected(true);
+        HideConsole.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                if (HideConsole.isSelected()) {
+                    consolePanel.setVisible(true);
+                } else {
+                    consolePanel.setVisible(false);
+                }
+                repaint();
+            }
+        });
+        HideTools.setSelected(true);
+        HideTools.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                if (HideTools.isSelected()) {
+                    toolsPanel.setVisible(true);
+                } else {
+                    toolsPanel.setVisible(false);
+                }
+                repaint();
+            }
+        });
+        HideDataPanel.setSelected(true);
+        HideDataPanel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                if (HideDataPanel.isSelected()) {
+                    dataPanel.setVisible(true);
+                } else {
+                    dataPanel.setVisible(false);
+                }
+                repaint();
+            }
+        });
+        Window.add(HideDataPanel);
+        Window.add(HideTools);
+        Window.add(HideConsole);
+        Window.addSeparator();
+        Window.add(Tile);
+        Window.add(Cascade);
+        Window.addSeparator();
+        Window.add(ClearConsole);
+        ClearConsole.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                consoleArea.setText("");
+            }
+        });
+        Tile.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                tile(workSpacePanel);
+            }
+        });
+        Cascade.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                cascade(workSpacePanel);
+            }
+        });
+
+        HelpM.add(Help);
+        HelpM.addSeparator();
+        HelpM.add(About);
+        Help.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+
+
+            }
+        });
+        About.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                final JEditorPane editorPane = new JEditorPane();
+
+                // Enable use of custom set fonts
+                editorPane.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, Boolean.TRUE);
+                editorPane.setFont(new Font("Verdana", Font.PLAIN, 14));
+
+                editorPane.setPreferredSize(new Dimension(470, 100));
+                editorPane.setEditable(false);
+                editorPane.setContentType("text/html");
+                editorPane.setText(
+                        "<html>"
+                        + "<body link=\"#009aff\" vlink=\"#009aff\" alink=\"#009aff\">"
+                        + "BiosynDesign - Best thing on Earth,  (c) SFB 2016"
+                        + "<br>Authors: Hiroyuki Kuwahara & Ramzan Umarov<br>"
+                        + "<a href=\"mailto:hkuwahara@gmail.com?Subject=BiosynDesign\" target=\"_top\"><br>"
+                        + "<font color=\"009aff\">Send Mail</font></a>"
+                        + "</body>"
+                        + "</html>");
+
+                editorPane.setBorder(BorderFactory.createEmptyBorder());
+                editorPane.setBackground(new Color(0, 0, 0, 0));
+                // TIP: Add Hyperlink listener to process hyperlinks
+                editorPane.addHyperlinkListener(new HyperlinkListener() {
+                    public void hyperlinkUpdate(final HyperlinkEvent e) {
+                        if (e.getEventType() == HyperlinkEvent.EventType.ENTERED) {
+                            EventQueue.invokeLater(new Runnable() {
+                                public void run() {
+                                    // TIP: Show hand cursor
+                                    SwingUtilities.getWindowAncestor(editorPane).setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                                    // TIP: Show URL as the tooltip
+                                    editorPane.setToolTipText(e.getURL().toExternalForm());
+                                }
+                            });
+                        } else if (e.getEventType() == HyperlinkEvent.EventType.EXITED) {
+                            EventQueue.invokeLater(new Runnable() {
+                                public void run() {
+                                    // Show default cursor
+                                    SwingUtilities.getWindowAncestor(editorPane).setCursor(Cursor.getDefaultCursor());
+
+                                    // Reset tooltip
+                                    editorPane.setToolTipText(null);
+                                }
+                            });
+                        } else if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                            // TIP: Starting with JDK6 you can show the URL in desktop browser
+                            if (Desktop.isDesktopSupported()) {
+                                try {
+                                    Desktop.getDesktop().browse(e.getURL().toURI());
+                                } catch (Exception ex) {
+                                }
+                            }
+                            //System.out.println("Go to URL: " + e.getURL());
+                        }
+                    }
+                });
+
+                JOptionPane.showMessageDialog(null,
+                        editorPane,
+                        "About",
+                        JOptionPane.INFORMATION_MESSAGE);
+
+            }
+        });
+
+
+
+        menu.add(File);
+        menu.add(Options);
+        menu.add(Window);
+        menu.add(HelpM);
+        // </editor-fold>
+        // <editor-fold defaultstate="collapsed" desc="panels">
+
+        dataSelectPanel = new JPanel();
+        dataSelectPanel.setBorder(BorderFactory.createEmptyBorder(0, panelMargin, 0, panelMargin));
+        GridLayout blayout = new GridLayout(3, 2);
+        blayout.setVgap(10);
+        blayout.setHgap(10);
+        dataSelectPanel.setLayout(blayout);
+
+        dataTransformPanel = new JPanel();
+
+        GridLayout dtpl = new GridLayout(6, 1);
+        dtpl.setVgap(10);
+        dtpl.setHgap(10);
+        dataTransformPanel.setLayout(dtpl);
+
+        // <editor-fold defaultstate="collapsed" desc="labels">
+        newProject = new JLabel();
+        newProject.setIcon(new ImageIcon(Main.class.getResource("images/new.png")));
+        newProject.setToolTipText("New Project");
+        newProject.addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            public void mousePressed(MouseEvent e) {
+            }
+
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            public void mouseEntered(MouseEvent e) {
+                newProject.setIcon(new ImageIcon(Main.class.getResource("images/newRollover.png")));
+            }
+
+            public void mouseExited(MouseEvent e) {
+                newProject.setIcon(new ImageIcon(Main.class.getResource("images/new.png")));
+            }
+        });
+
+        openProject = new JLabel();
+        openProject.setIcon(new ImageIcon(Main.class.getResource("images/open.png")));
+        openProject.setToolTipText("Open Project");
+        openProject.addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            public void mousePressed(MouseEvent e) {
+            }
+
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            public void mouseEntered(MouseEvent e) {
+                openProject.setIcon(new ImageIcon(Main.class.getResource("images/openRollover.png")));
+            }
+
+            public void mouseExited(MouseEvent e) {
+                openProject.setIcon(new ImageIcon(Main.class.getResource("images/open.png")));
+            }
+        });
+
+        saveProject = new JLabel();
+        saveProject.setIcon(new ImageIcon(Main.class.getResource("images/save.png")));
+        saveProject.setToolTipText("Save Project");
+        saveProject.addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            public void mousePressed(MouseEvent e) {
+            }
+
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            public void mouseEntered(MouseEvent e) {
+                saveProject.setIcon(new ImageIcon(Main.class.getResource("images/saveRollover.png")));
+            }
+
+            public void mouseExited(MouseEvent e) {
+                saveProject.setIcon(new ImageIcon(Main.class.getResource("images/save.png")));
+            }
+        });
+
+
+
+        toolsPanel = new JToolBar();
+
+        toolsPanel.setLayout(new BoxLayout(toolsPanel, BoxLayout.X_AXIS));
+        toolsPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+        toolsPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        toolsPanel.add(newProject);
+        toolsPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        toolsPanel.add(openProject);
+        toolsPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        toolsPanel.add(saveProject);
+        toolsPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        toolsPanel.addSeparator();
+        //</editor-fold>
+
+        dataPanel = new JPanel();
+        dataPanel.setLayout(new BoxLayout(dataPanel, BoxLayout.PAGE_AXIS));
+        dataPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+        dataPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        JPanel labelPan1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        labelPan1.add(new JLabel("Stuff"));
+
+        dataPanel.add(labelPan1);
+
+        dataPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+
+        workSpacePanel = new JDesktopPane();
+        workSpacePanel.setBackground(Color.GRAY);
+        workSpacePanel.setBorder(BorderFactory.createEmptyBorder());
+        // </editor-fold>
+        // <editor-fold desc="console">
+        consoleArea = new JTextArea();
+        consoleArea.setEditable(false);
+        consoleArea.setLineWrap(true);
+        consoleArea.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        consoleScroll = new JScrollPane();
+        consoleScroll.setPreferredSize(new Dimension(250, 110));
+        consoleScroll.setViewportView(consoleArea);
+        consoleScroll.setBorder(BorderFactory.createEmptyBorder());
+
+        consolePanel = new JPanel();
+        consolePanel.setLayout(new BorderLayout());
+        consolePanel.add(consoleScroll, BorderLayout.CENTER);
+        consolePanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));// </editor-fold>
+        // <editor-fold defaultstate="collapsed" desc="prepairing frame">
+        setTitle("VISAN");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setJMenuBar(menu);
+        add(toolsPanel, BorderLayout.NORTH);
+        add(dataPanel, BorderLayout.EAST);
+        add(consolePanel, BorderLayout.SOUTH);
+        add(workSpacePanel, BorderLayout.CENTER);
+        // </editor-fold>
+    }
+
+    void disposeAllIFrames() {
+        for (JInternalFrame iframe : workSpacePanel.getAllFrames()) {
+            iframe.dispose();
+        }
+    }
+
+    public static void tile(JDesktopPane desktopPane) {
+        JInternalFrame[] frames = desktopPane.getAllFrames();
+        if (frames.length == 0) {
+            return;
+        }
+        tile(frames, desktopPane.getBounds());
+    }
+
+    private static void tile(JInternalFrame[] frames, Rectangle dBounds) {
+        int cols = (int) Math.sqrt(frames.length);
+        int rows = (int) (Math.ceil(((double) frames.length) / cols));
+        int lastRow = frames.length - cols * (rows - 1);
+        int width, height;
+
+        if (lastRow == 0) {
+            rows--;
+            height = dBounds.height / rows;
+        } else {
+            height = dBounds.height / rows;
+            if (lastRow < cols) {
+                rows--;
+                width = dBounds.width / lastRow;
+                for (int i = 0; i < lastRow; i++) {
+                    frames[cols * rows + i].setBounds(i * width, rows * height,
+                            width, height);
+                }
+            }
+        }
+
+        width = dBounds.width / cols;
+        for (int j = 0; j < rows; j++) {
+            for (int i = 0; i < cols; i++) {
+                frames[i + j * cols].setBounds(i * width, j * height,
+                        width, height);
+            }
+        }
+    }
+
+    public static void cascade(JDesktopPane desktopPane) {
+        JInternalFrame[] frames = desktopPane.getAllFrames();
+        if (frames.length == 0) {
+            return;
+        }
+
+        cascade(frames, desktopPane.getBounds(), 24);
+    }
+
+    private static void cascade(JInternalFrame[] frames, Rectangle dBounds, int separation) {
+        int margin = frames.length * separation + separation;
+        int width = dBounds.width - margin;
+        int height = dBounds.height - margin;
+        for (int i = 0; i < frames.length; i++) {
+            frames[i].setBounds(separation + dBounds.x + i * separation,
+                    separation + dBounds.y + i * separation,
+                    width, height);
+        }
+    }
+
+
+
+    void writeToConsole(String text) {
+        consoleArea.append(text);
+    }
+
+    void createFrame(String title, final JComponent c) {
+        JScrollPane sp = new JScrollPane(c);
+        sp.addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent e) {
+                c.dispatchEvent(e);
+
+            }
+
+            public void mousePressed(MouseEvent e) {
+            }
+
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            public void mouseExited(MouseEvent e) {
+            }
+        });
+        MyInternalFrame frame = new MyInternalFrame(title, sp);
+        frame.setVisible(true);
+        frame.addInternalFrameListener(new InternalFrameListener() {
+            public void internalFrameOpened(InternalFrameEvent e) {
+            }
+
+            public void internalFrameClosing(InternalFrameEvent e) {
+            }
+
+            public void internalFrameClosed(InternalFrameEvent e) {
+                Main.removeFromComponentList(c);
+            }
+
+            public void internalFrameIconified(InternalFrameEvent e) {
+            }
+
+            public void internalFrameDeiconified(InternalFrameEvent e) {
+            }
+
+            public void internalFrameActivated(InternalFrameEvent e) {
+                Main.setSelectedComponent(c);
+            }
+
+            public void internalFrameDeactivated(InternalFrameEvent e) {
+            }
+        });
+        workSpacePanel.add(frame);
+        try {
+            frame.setSelected(true);
+        } catch (PropertyVetoException e) {
+        }
+    }
+
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,38 @@
 package main;
 
 
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class Main {
+
+    private static GUI mainWindow;
+    private static ArrayList<Object> componentList;
+    static Object selectedComponent;
+    private static JFileChooser fc;
+
     public static void main(String[] args){
-        System.out.println("Hello1");
-        System.out.println("Hello2");
-        System.out.println("Hello3");
+        componentList = new ArrayList<Object>();
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+        }
+        mainWindow = new GUI();
+        mainWindow.setTitle("BiosynDesign");
+        mainWindow.setVisible(true);
+        fc = new JFileChooser();
     }
+
+    static void removeFromComponentList(Object c) {
+        componentList.remove(c);
+    }
+
+    static void setSelectedComponent(Object c) {
+        selectedComponent = c;
+    }
+
+    public static void writeToConsole(String text) {
+        mainWindow.writeToConsole(text);
+    }
+
 }
